14cdcd5bb396972e2c94fc76ad310378a6d39b93
sqlnatan <natangabrielra@gmail.com>
2025-04-28 15:36:54 -0300

    removendo splashscreen do android para usar apenas a do app

--- app/src/main/java/com/pillora/pillora/MainActivity.kt
+++ app/src/main/java/com/pillora/pillora/MainActivity.kt
@@ -14,6 +14,7 @@ import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Pillora)
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
         val analytics = Firebase.analytics
