567bbaf6b4e503967c126157d2d4733f197d843c
sqlnatan <natangabrielra@gmail.com>
2026-01-06 00:28:37 -0300

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.pillora.pillora"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
